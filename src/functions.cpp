0144ab00d965063427c146e1bc7872e3ad157c08
bardt88 <bartlaan@gmail.com>
2015-02-16 22:41:05 +0100

    -

--- src/functions.cpp
+++ src/functions.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
-void test()
+void getNumbers()
 {
     int x = 0;
-    cout << "enter a number" << endl;
+    cout << "int" << endl;
     cin >> x; // read number from console and store it in x
+    double y = 0;
+    cout << "double" << endl;
+    cin >> y;
+    long z = 0;
+    cout << "long" << endl;
+    cin >> z;
+    float z2 = 0;
+    cout << "long" << endl;
+    cin >> z2;
+
+
     cout << "You entered " << x << endl;
+    cout << "You entered " << y << endl;
+    cout << "You entered " << z << endl;
+    cout << "You entered " << z2 << endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 
-    test();
+    getNumbers();
     return 0;
 }
 
